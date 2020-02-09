4e47341a5aa5fa81495700ec87abb1d3c348d68d
Udi Talias <udi.talias@gmail.com>
2020-02-09 12:23:39 +0200

    Release 1.0.12

--- typings/utils/metadata.d.ts
+++ typings/utils/metadata.d.ts
@@ -1,11 +1,11 @@
 import { IDefinitionMetadata } from "../interfaces";
-export declare type MetadataHandlers = {
-    ensureMetadata: (target: any) => IDefinitionMetadata;
-    setMetadata: (target: any, key: keyof IDefinitionMetadata, value: any) => void;
-    getMetadata: (target: any) => IDefinitionMetadata;
+export declare type MetadataHandlers<T> = {
+    ensureMetadata: (target: any) => T;
+    setMetadata: (target: any, key: keyof T, value: any) => void;
+    getMetadata: (target: any) => T;
     hasMetadata: (target: any) => boolean;
-    pushMetadata: (target: any, key: keyof IDefinitionMetadata, value: any) => void;
+    pushMetadata: (target: any, key: keyof T, value: any) => void;
 };
-export declare function createMetadataHandlers(metadataKey: symbol): MetadataHandlers;
-declare const _default: MetadataHandlers;
+export declare function createMetadataHandlers<T = any>(metadataKey: symbol): MetadataHandlers<T>;
+declare const _default: MetadataHandlers<IDefinitionMetadata>;
 export default _default;
