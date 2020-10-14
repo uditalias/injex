5e9201f7c4626f38b81ce2aae5137ea85f94af10
Udi Talias <udi.talias@gmail.com>
2020-10-14 19:57:43 +0300

    test(module): express-plugin - change mock server port

--- plugins/express-plugin/__tests__/plugin.test.ts
+++ plugins/express-plugin/__tests__/plugin.test.ts
@@ -4,6 +4,8 @@ import * as path from "path";
 import { Injex } from "@injex/node";
 import * as http from "http";
 
+const PORT = 8081;
+
 function get(url): Promise<string> {
 	return new Promise((resolve) => {
 		http.get(url, (res) => {
@@ -27,7 +29,7 @@ describe("Plugin", () => {
 	it("should create plugin with express routes and middlewares (e2e)", async () => {
 
 		const app = express();
-		const server = app.listen(8080);
+		const server = app.listen(PORT);
 
 		const expressPlugin = new ExpressPlugin({
 			app
@@ -48,18 +50,18 @@ describe("Plugin", () => {
 
 		await container.bootstrap();
 
-		const homeResponse = await get("http://localhost:8080/");
+		const homeResponse = await get(`http://localhost:${PORT}/`);
 		expect(homeResponse).toBe("<h1>Welcome home!</h1>");
 
-		const categoryResponse = await get("http://localhost:8080/cat/lifestyle");
+		const categoryResponse = await get(`http://localhost:${PORT}/cat/lifestyle`);
 		expect(categoryResponse).toBe("<h1>Welcome to lifestyle category</h1>");
 
 		// test middleware failure
-		const cartResponseUnauthorized = await get("http://localhost:8080/cart");
+		const cartResponseUnauthorized = await get(`http://localhost:${PORT}/cart`);
 		expect(cartResponseUnauthorized).toBe("unauthorize");
 
 		// test middleware pass
-		const cartResponseAuthorized = await get("http://localhost:8080/cart?token=123456");
+		const cartResponseAuthorized = await get(`http://localhost:${PORT}/cart?token=123456`);
 		expect(cartResponseAuthorized).toBe("<h1>This is your cart</h1>");
 
 		server.close();
