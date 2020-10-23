883b97ce89ec3f5c0f82d8eab8fa5c871ba071a0
Udi Talias <udi.talias@gmail.com>
2020-10-24 02:05:34 +0300

    fix(module): dependencies

--- examples/injex-react-plugin-example/src/components/avatar/Avatar.tsx
+++ examples/injex-react-plugin-example/src/components/avatar/Avatar.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import * as React from "react";
 import theme from "./theme.scss";
 
-function Avatar({ url, size = 50, className = "" }: { url: string; size: number; className: string }): JSX.Element {
+function Avatar({ url, size = 50, className = "" }: { url: string; size?: number; className?: string }): JSX.Element {
     return (
         <div className={clsx(theme.avatar, className)} style={{ width: size, height: size }} >
             <img src={url} />
