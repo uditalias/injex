d302cb980102b46a0b98c845e43c9faab11b0637
Udi Talias <udi.talias@gmail.com>
2020-12-02 18:14:18 +0200

    fix(module): core - AliasFactory args list typings

--- packages/core/src/interfaces.ts
+++ packages/core/src/interfaces.ts
@@ -62,5 +62,5 @@ export interface ILazyModule<T> {
     import(...args: any[]): Promise<IConstructor<T>>;
 }
 
-export type AliasFactory<K extends string, T> = { [key in K]: () => T };
+export type AliasFactory<K extends string, T> = { [key in K]: (...args: any[]) => T };
 export type AliasMap<K extends string, T> = { [key in K]: T };
