cd68312769ce6daa75f63f62b9796439874f6fb1
Udi Talias <udi.talias@gmail.com>
2021-01-21 15:03:45 +0200

    fix(client): better webpack v5 duplication hanling

--- runtimes/webpack/src/injex.ts
+++ runtimes/webpack/src/injex.ts
@@ -21,8 +21,7 @@ export default class InjexWebpack extends Injex<IWebpackContainerConfig> {
         const allContextFiles = requireContext.keys().reduce((files, currentFile) => {
             // webpack v5 has a breaking change when the 'requireContext.keys()' may have duplications
             // https://github.com/webpack/webpack/issues/12087
-            currentFile = currentFile.replace(/^.\//, "");
-            return files.includes(currentFile) ? files : files.concat(currentFile);
+            return /^.\//.test(currentFile) ? files : files.concat(currentFile);
         }, []);
 
         allContextFiles.forEach((filePath) => this.registerModuleExports(requireContext(filePath)));
