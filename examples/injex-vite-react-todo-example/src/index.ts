1d5496b62890aa609a1bdd5bef740295acfdb76c
Udi Talias ⚛️ <udi.talias@gmail.com>
2022-09-08 11:58:08 +0300

    fix(client): ts-ignore for import.meta.glob (fails on Netlify)

--- examples/injex-vite-react-todo-example/src/index.ts
+++ examples/injex-vite-react-todo-example/src/index.ts
@@ -6,5 +6,9 @@ configure({
 });
 
 Injex.create({
+    // This line fails on Netlify's build
+    // error TS2339: Property 'glob' does not exist on type 'ImportMeta'.
+    // Adding @ts-ignore flag for now to make it work.
+    // @ts-ignore
     glob: () => import.meta.glob('./**/*.{ts,tsx}', { eager: true })
 }).bootstrap();
