71228900d1f492eb4ee62726f82403a2fffd0f8c
Udi Talias ⚛️ <udi.talias@gmail.com>
2022-12-28 14:46:58 +0200

    feat(utils): handle yieldToMain when browser tab is idle

--- packages/stdlib/src/utils.ts
+++ packages/stdlib/src/utils.ts
@@ -1,3 +1,12 @@
+const _document = (function () {
+    try {
+        return window.document;
+    } catch (e) {
+        // this is not a browser context
+        return null;
+    }
+})();
+
 export function toCamelCase(str: string): string {
     return str[0].toLowerCase() + str.slice(1);
 }
@@ -15,5 +24,9 @@ export function getConstructorName(instance: any): string {
 }
 
 export function yieldToMain(): Promise<void> {
+    if (_document?.hidden) {
+        return Promise.resolve();
+    }
+
     return new Promise((resolve) => setTimeout(resolve, 0));
 }
